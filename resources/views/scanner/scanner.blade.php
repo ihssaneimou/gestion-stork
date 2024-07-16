5a8e9104dd140bbcaea7331a253dd82f3eb2db7d
youssefmaimouni <135459547+youssefmaimouni@users.noreply.github.com>
2024-07-16 15:02:04 +0100

    cc

--- resources/views/scanner/scanner.blade.php
+++ resources/views/scanner/scanner.blade.php
@@ -1,5 +1,5 @@
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js" integrity="sha512-r6rDA7W6ZeQhvl8S7yRVQUKVHdexq+GAlNkNNqVC7YyIV+NwqCTJe2hDWCiffTyRNOeGEzRRJ9ifvRm/HCzGYg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<x-nav-bar>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js" integrity="sha512-r6rDA7W6ZeQhvl8S7yRVQUKVHdexq+GAlNkNNqVC7YyIV+NwqCTJe2hDWCiffTyRNOeGEzRRJ9ifvRm/HCzGYg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
     <div style="text-align: center;">
 
         <div id="reader" style="width: 800px;"></div>
@@ -20,3 +20,4 @@
         const config = {fps:20, qrbox:{width:500, height:500}}
         html5Qrcode.start({facingMode:"environment"}, config,qrCodeSuccessCallback );
     </script>
+</x-nav-bar>
