69bae932939b399cf6c921a5043586405fb1b4cd
youssefmaimouni <135459547+youssefmaimouni@users.noreply.github.com>
2024-07-17 10:54:25 +0100

    "Updated scanner views: reformatted HTML structure, adjusted CSS classes, and modified JavaScript code for QR code scanning functionality."

--- resources/views/scanner/scanner.blade.php
+++ resources/views/scanner/scanner.blade.php
@@ -1,30 +1,26 @@
 <x-nav-bar>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js" integrity="sha512-r6rDA7W6ZeQhvl8S7yRVQUKVHdexq+GAlNkNNqVC7YyIV+NwqCTJe2hDWCiffTyRNOeGEzRRJ9ifvRm/HCzGYg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <div style="text-align: center;">
-
-        <div id="reader" style="width: 800px;"></div>
-        <div id="show" style="display: none;">
-            <h4>Scanned Result</h4>
-            <p style="color: blue;" id="result"></p>
-        </div>
+        <h2>Scanner un QR Code</h2>
+        <div id="read" class="w-96 sm:w-[500px] m-auto "></div>
     </div>
     <script>
-        const html5Qrcode = new Html5Qrcode('reader');
+        const html5Qrcode = new Html5Qrcode('read');
+
         const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+            console.log("QR Code scanned successfully:", decodedText);
             if (decodedText) {
-                document.getElementById('show').style.display = 'block';
-                document.getElementById('result').textContent = decodedText;
+                const url = `/marchandise-info/${decodedText}`;
+                alert(url);
+                window.location.replace(url);
                 html5Qrcode.stop();
             }
         }
-        const config = {
-            fps: 20,
-            qrbox: {
-                width: 500,
-                height: 500
-            }
-        }
-        html5Qrcode.start({
-            facingMode: "environment"
-        }, config, qrCodeSuccessCallback);
+
+        const config = { fps: 20, qrbox: { width: 250, height: 250 } }
+        html5Qrcode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback)
+            .catch(err => {
+                console.error("Error starting QR code scanner:", err);
+            });
     </script>
 </x-nav-bar>
